3b809efede79584a7a34714cebd0130177819e25
Ian MacLeod <ian@nevir.net>
2020-04-07 16:33:34 -0700

    Follow FLinkerLoad

--- packages/pak-parser/src/ObjectFile.ts
+++ packages/pak-parser/src/ObjectFile.ts
@@ -1,3 +1,4 @@
+import { TArray } from './containers';
 import { Int32 } from './primitive';
 import { Reader } from './readers';
 import { FAssetData } from './structs/FAssetData';
@@ -17,10 +18,12 @@ import { Shape } from './util/parsers';
  */
 export class ObjectFile {
   summary!: Shape<typeof FPackageFileSummary>;
-  names!: NameMap;
-  imports!: Shape<typeof FObjectImport>[];
-  exports!: Shape<typeof FObjectExport>[];
-  softPackageReferences!: string[];
+  names = [] as NameMap;
+  imports = [] as Shape<typeof FObjectImport>[];
+  exports = [] as Shape<typeof FObjectExport>[];
+  depends = [] as number[][];
+  preloadDependencies = [] as number[];
+  softPackageReferences?: string[];
   assetData?: Shape<typeof FAssetData>;
 
   constructor(public filename: string, private reader: Reader, public entry: Shape<typeof FPakEntry>) {}
@@ -31,35 +34,95 @@ export class ObjectFile {
     await this.loadNameMap();
     await this.loadImports();
     await this.loadExports();
+    await this.fixupImports();
+    await this.fixupExports();
+    await this.loadDepends();
+    await this.loadPreloadDependencies();
+    await this.loadThumbnails();
     await this.loadSoftPackageReferences();
     await this.loadSearchableNames();
     await this.loadAssetRegistryData();
   }
 
+  // https://github.com/SatisfactoryModdingUE/UnrealEngine/blob/4.22-CSS/Engine/Source/Runtime/CoreUObject/Private/UObject/LinkerLoad.cpp#L1130-L1379
   async loadSummary() {
     this.summary = await this.reader.read(FPackageFileSummary);
   }
 
+  // https://github.com/SatisfactoryModdingUE/UnrealEngine/blob/4.22-CSS/Engine/Source/Runtime/CoreUObject/Private/UObject/LinkerLoad.cpp#L1381-L1440
   async loadNameMap() {
     const { nameOffset, nameCount } = this.summary;
+    if (!nameOffset || !nameCount) return;
+
     this.reader.seekTo(nameOffset);
     this.names = await this.reader.readList(nameCount, FNameEntrySerialized);
   }
 
+  // https://github.com/SatisfactoryModdingUE/UnrealEngine/blob/4.22-CSS/Engine/Source/Runtime/CoreUObject/Private/UObject/LinkerLoad.cpp#L1442-L1473
+  async loadGatherableTextData() {
+    const { gatherableTextDataOffset, gatherableTextDataCount } = this.summary;
+    if (!gatherableTextDataOffset || !gatherableTextDataCount) return;
+
+    throw new Error(`Please implement ObjectFile#loadGatherableTextData`);
+  }
+
+  // https://github.com/SatisfactoryModdingUE/UnrealEngine/blob/4.22-CSS/Engine/Source/Runtime/CoreUObject/Private/UObject/LinkerLoad.cpp#L1475-L1498
   async loadImports() {
     const { importOffset, importCount } = this.summary;
+    if (!importOffset || !importCount) return;
+
     this.reader.seekTo(importOffset);
     this.imports = await this.reader.readList(importCount, FObjectImport(this.names));
   }
 
+  // https://github.com/SatisfactoryModdingUE/UnrealEngine/blob/4.22-CSS/Engine/Source/Runtime/CoreUObject/Private/UObject/LinkerLoad.cpp#L1681-L1707
   async loadExports() {
     const { exportOffset, exportCount } = this.summary;
+    if (!exportOffset || !exportCount) return;
+
     this.reader.seekTo(exportOffset);
     this.exports = await this.reader.readList(exportCount, FObjectExport(this.names));
   }
 
+  // https://github.com/SatisfactoryModdingUE/UnrealEngine/blob/4.22-CSS/Engine/Source/Runtime/CoreUObject/Private/UObject/LinkerLoad.cpp#L1500-L1679
+  async fixupImports() {
+    // TODO: Do we need to do anything here?
+  }
+
+  // https://github.com/SatisfactoryModdingUE/UnrealEngine/blob/4.22-CSS/Engine/Source/Runtime/CoreUObject/Private/UObject/LinkerLoad.cpp#L5209-L5347
+  async fixupExports() {
+    // TODO: Do we need to do anything here?
+  }
+
+  // https://github.com/SatisfactoryModdingUE/UnrealEngine/blob/4.22-CSS/Engine/Source/Runtime/CoreUObject/Private/UObject/LinkerLoad.cpp#L1709-L1752
+  async loadDepends() {
+    const { dependsOffset, exportCount } = this.summary;
+
+    this.reader.seekTo(dependsOffset);
+    this.depends = await this.reader.readList(exportCount, TArray(Int32));
+  }
+
+  // https://github.com/SatisfactoryModdingUE/UnrealEngine/blob/4.22-CSS/Engine/Source/Runtime/CoreUObject/Private/UObject/LinkerLoad.cpp#L1754-L1800
+  async loadPreloadDependencies() {
+    const { preloadDependencyCount, preloadDependencyOffset } = this.summary;
+    if (!preloadDependencyCount || !preloadDependencyOffset) return;
+
+    this.reader.seekTo(preloadDependencyOffset);
+    this.preloadDependencies = await this.reader.readList(preloadDependencyCount, Int32);
+  }
+
+  // https://github.com/SatisfactoryModdingUE/UnrealEngine/blob/4.22-CSS/Engine/Source/Runtime/CoreUObject/Private/UObject/LinkerLoad.cpp#L1802-L1905
+  async loadThumbnails() {
+    const { thumbnailTableOffset } = this.summary;
+    if (!thumbnailTableOffset) return;
+
+    throw new Error(`Please implement ObjectFile#loadThumbnails`);
+  }
+
   async loadSoftPackageReferences() {
     const { softPackageReferencesOffset, softPackageReferencesCount } = this.summary;
+    if (!softPackageReferencesOffset || !softPackageReferencesCount) return;
+
     this.reader.seekTo(softPackageReferencesOffset);
     this.softPackageReferences = await this.reader.readList(softPackageReferencesCount, FName(this.names));
   }
