e31da0a00bf3db878e4bd844dc084d820c0444ec
Alex <aafu@ucdavis.edu>
2020-05-21 15:04:52 -0400

    Generate a manifest every time

--- pipeline/interfaces-to-json/main.ts
+++ pipeline/interfaces-to-json/main.ts
@@ -3,7 +3,9 @@ import * as fs from 'fs-extra';
 import * as glob from 'glob';
 import * as path from 'path';
 import * as TJS from 'typescript-json-schema';
-
+// import * as jsonImportTest from '../../.data-landing/json';
+//
+// console.log(Object.keys(jsonImportTest));
 parseAll(process.argv[2], process.argv[3]);
 
 function shallowClone(sourceDict) {
@@ -39,7 +41,7 @@ function parseAll(sourceDir = paths.dataLanding.interfaces, destDir = paths.data
   const program = TJS.getProgramFromFiles(glob.sync(globExpression), compilerOptions);
   const generator = TJS.buildGenerator(program, settings);
 
-  const createdDirs = new Set();
+  const filesByDir = new Map<string, string[]>();
 
   for (const header of glob.sync(globExpression)) {
     if (header.endsWith('/index.ts')) continue;
@@ -52,10 +54,11 @@ function parseAll(sourceDir = paths.dataLanding.interfaces, destDir = paths.data
     );
 
     const fullPath = path.join(destDir, newFilePath);
-    if (!createdDirs.has(path.dirname(fullPath))) {
+    if (!filesByDir.has(path.dirname(fullPath))) {
       fs.mkdirSync(path.dirname(fullPath), { recursive: true });
-      createdDirs.add(path.dirname(fullPath));
+      filesByDir.set(path.dirname(fullPath), []);
     }
+    filesByDir.get(path.dirname(fullPath)).push(newFilePath);
 
     const schema = generator.getSchemaForSymbol(path.basename(portedPath).replace(/\.ts$/g, ''));
     const schemaString = JSON.stringify(shallowClone(schema), null, 2);
@@ -64,18 +67,42 @@ function parseAll(sourceDir = paths.dataLanding.interfaces, destDir = paths.data
     fs.writeFileSync(fullPath, schemaString);
   }
 
+  for (const parentDir of filesByDir.keys()) {
+    const children = filesByDir.get(parentDir)!;
+
+    const indexLines = [];
+
+    for (const child of children) {
+      if (path.parse(child).name.includes('.')) {
+        throw new Error('Filename has an illegal character: ' + child);
+      }
+
+      indexLines.push(`export * as ${path.parse(child).name + 'JSON'} from './${path.basename(child)}';`);
+    }
+
+    fs.writeFileSync(path.join(parentDir, 'index.ts'), indexLines.join('\n'));
+  }
+
   const nativeProgram = TJS.getProgramFromFiles(glob.sync(nativeGlobExpression), compilerOptions);
   const nativeGenerator = TJS.buildGenerator(nativeProgram, settings);
 
   const allSymbols = nativeGenerator.getMainFileSymbols(nativeProgram, glob.sync(nativeGlobExpression));
 
-  fs.mkdirSync(path.join(destDir, 'native'), { recursive: true });
+  const nativeDir = path.join(destDir, 'native');
+
+  fs.mkdirSync(nativeDir, { recursive: true });
+
+  const nativeIndexLines = [];
 
   for (const symbol of allSymbols) {
-    const newPath = path.join(destDir, 'native', `${symbol}.json`);
+    const newPath = path.join(nativeDir, `${symbol}.json`);
     try {
       const schema = generator.getSchemaForSymbol(symbol);
       const schemaString = JSON.stringify(shallowClone(schema), null, 2);
+
+      nativeIndexLines.push(
+        `export * as ${path.parse(newPath).name + 'JSON'} from './${path.basename(newPath)}';`,
+      );
 
       process.stderr.write(`\u001b[2Kconverting: ${newPath}\r`);
       fs.writeFileSync(newPath, schemaString);
@@ -83,4 +110,18 @@ function parseAll(sourceDir = paths.dataLanding.interfaces, destDir = paths.data
       process.stderr.write(`\u001b[2Kfailure writing: ${newPath}\r`);
     }
   }
+
+  fs.writeFileSync(path.join(nativeDir, 'index.ts'), nativeIndexLines.join('\n'));
+
+  const allSubDirs = [...filesByDir.keys(), nativeDir];
+
+  const relativeDirs = allSubDirs.map(dir => path.relative(destDir, dir));
+
+  const masterIndexLines = [];
+
+  relativeDirs.forEach(relDir => {
+    masterIndexLines.push(`export * from './${relDir}';`);
+  });
+
+  fs.writeFileSync(path.join(destDir, 'index.ts'), masterIndexLines.join('\n'), 'utf-8');
 }
